db70e95dabd1a4382b71813eb37cc99497ea6591
xiefei <xiefei@sensetime.com>
2019-05-09 20:23:41 +0800

    add ubuntu install

---
+++ package/ubuntu/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# setup install command
+incc="apt install -y "
+
+# basic
+# git : repo management tool
+# zsh : a wonderful command line interpreter
+# jq  : json parser
+$incc git jq zsh
