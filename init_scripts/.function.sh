04fcdf194f26a8be42d5b58a7d30ef4d5d9a87b0
xiefei <xiefei@sensetime.com>
2019-05-10 21:30:59 +0800

    add dot file, scripts

---
+++ init_scripts/.function.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# run one command for n times
+# example: run_n_times 4 ls : run command "ls" for 4 times.
+function run_n_times() {
+    number=$1
+    shift
+    for n in $(seq $number); do
+      $@
+    done
+}
+
--- package/ubuntu/install.sh
+++ package/ubuntu/install.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
 # setup install command
-incc="apt install -y "
+incc="sudo apt install -y "
+update_cc="sudo apt update"
 
 # basic
 # git : repo management tool
-# zsh : a wonderful command line interpreter
 # jq  : json parser
-$incc git jq zsh
+# tig : good git tool
+$incc git jq tig 
+
+# basic edit tool
+$incc vim vi
+
+# media tool
+$incc ffmpeg
+
+# basic net tools
+$incc curl wget
+
+# command: add-apt-repository
+$incc software-properties-common
+# command: sudo
+$incc sudo
+
+# zsh
+# zsh : a wonderful command line interpreter
+$incc zsh
+
+# fasd
+# fasd : jump directory tool
+# install dep: add-apt-repository
+sudo add-apt-repository ppa:aacebedo/fasd -y
+$update_cc
+$incc fasd
