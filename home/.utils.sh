1fbae7c24773d9910b5fdee51c383243de4b18ff
xiefei <xiefei@sensetime.com>
2019-09-23 18:08:06 +0800

    fix sed use in different system

--- home/.utils.sh
+++ home/.utils.sh
@@ -5,5 +5,9 @@ clean_file_between_flags() {
     file_name=$1
     start_flag=$2
     end_flag=$3
-    sed  -i '' "/$start_flag/,/$end_flag/d" $file_name 
+    if [ "$(uname)" == "Linux" ]; then
+        sed  -i  "/$start_flag/,/$end_flag/d" $file_name 
+    else
+        sed  -i '' "/$start_flag/,/$end_flag/d" $file_name 
+    fi
 }
