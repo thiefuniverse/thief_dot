4a1b71aef1ba628485bd5e2a7ec1cf61d08843f0
xiefei <xiefei@sensetime.com>
2019-10-22 22:09:03 +0800

    remove alias.config.sh

--- home/alias/alias.config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# THIEF_HOME_PATH is your dot file root path.
-vim=vim -u /big_repo/high_soft/thief_dot/home/vim/vimrc
-ts=setup.sh
-sz=source ~/.zshrc
-sb=source ~/.bashrc
-sbp=source ~/.bash_profile
-
