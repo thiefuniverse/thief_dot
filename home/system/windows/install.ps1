34114cbd9ed9e58455fa0a5cc69d96776963205c
thiefuniverse <thiefuniverses@gmail.com>
2022-01-23 18:25:40 +0800

    add windows install software config

---
+++ home/system/windows/install.ps1
@@ -0,0 +1,20 @@
+# if not administrator, we start new process with administrator
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){ Start-Process powershell.exe " -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+# install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# install software
+# programing
+choco install ninja llvm microsoft-windows-terminal powershell-core v2rayn vscode sumatrapdf git git-lfs.install cmake -y
+
+# system
+choco install universal-usb-installer -y
+
+# music, video
+choco install netease-cloudmusic potplayer telegram itunes powertoys -y
+
+# tools
+choco install everything windbg processhacker.portable obs-studio f.lux.install blender googlechrome teamviewer -y
+
+Pause
